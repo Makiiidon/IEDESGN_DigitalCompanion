a8c6d6e1de9207433b0265845cceb6bc0f56bb0c
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2023-03-09 02:11:07 +0800

    Added the buttons for the ingredients

---
+++ Assets/Scripts/AddIngredient.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddIngredient : MonoBehaviour
+{
+    [SerializeField] Enums.IngredientsTypes ingredient;
+    Crafting craftingHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        craftingHandler = Crafting.Instance;
+    }
+
+    public void Add()
+    {
+        craftingHandler.AddIngredient(ingredient);
+        Debug.Log(ingredient + " has been added!");
+    }
+
+    
+}
--- Assets/Scripts/Crafting.cs
+++ Assets/Scripts/Crafting.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Crafting : MonoBehaviour
 {
     [SerializeField] List<Enums.IngredientsTypes> ingredients;
     [SerializeField] List<Enums.Potions> potionOutput;
-   public void CheckAvailablePotions()
+    [SerializeField] GameObject PotionUI;
+    [SerializeField] TMP_Text PotionTextsUI;
+
+    public static Crafting Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        PotionUI.SetActive(false);
+    }
+
+    // Calls when you craft a potion
+    public void CheckAvailablePotions()
    {
         // Check if the player has the list of ingredients
 
@@ -63,5 +82,41 @@ public class Crafting : MonoBehaviour
             Debug.Log(potion);
 
         }
+
+        PotionUI.SetActive(true);
+        DisplayPotions();
     }
+    
+    // Calls when you want to empty the Lists
+    public void Clear()
+    {
+        ingredients.Clear();
+        potionOutput.Clear();
+        PotionUI.SetActive(false);
+    }
+
+    void DisplayPotions()
+    {
+        string temp = "";
+        foreach (Enums.Potions potion in potionOutput)
+        {
+            temp += potion.ToString() + "\n";
+        }
+
+        PotionTextsUI.SetText(temp);
+    }
+
+
+
+    // Getter
+    public List<Enums.Potions> GetPotions() { return potionOutput; }
+
+
+    // Setter
+    public void AddIngredient(Enums.IngredientsTypes ingredientToAdd)
+    {
+        ingredients.Add(ingredientToAdd);
+    }
+
+
 }
